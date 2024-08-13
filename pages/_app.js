eedcd9c7774b7cd355f5f8ccfbd382aa120a2e79
Mayur Samrutwar <samrutwarmayur1@gmail.com>
2024-08-14 02:25:23 +0530

    add: title

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
 import Layout from '@/components/layout/Layout'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <title>Phord | Afford the Premium Real Estate</title>
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
