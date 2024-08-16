cbe6847f4b4b92a82d26665ed90f4ccea090f00e
Mayur Samrutwar <samrutwarmayur1@gmail.com>
2024-08-16 16:03:34 +0530

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -4,24 +4,30 @@ import abi from "../contracts/abi/hello.json";
 
 export default function Test() {
   const contractAddress = "0xd9145CCE52D386f254917e481eB44e9943F39138";
+  const [message, setMessage] = useState("");
+
+  const { data, isError, isLoading } = useReadContracts({
+    contracts: [
+      {
+        address: contractAddress,
+        abi: abi,
+        functionName: "getMessage",
+      },
+    ],
+  });
 
   function handleRead() {
-    const result = useReadContracts({
-      contracts: [
-        {
-          address: contractAddress,
-          abi: abi,
-          functionName: "getMessage",
-        },
-      ],
-    });
-
-    console.log(result);
+    if (data && data[0]) {
+      setMessage(data[0].result);
+    }
   }
 
   return (
     <div>
       <button onClick={handleRead}>Update Message</button>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error reading contract</p>}
+      {message && <p>Message: {message}</p>}
     </div>
   );
 }
